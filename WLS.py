8c49304c18945da30a1e3e2150ccfb38a68b2b31
formaaaa <wieczorek.konrad@gmail.com>
2023-03-15 15:37:52 +0200

    finished the WLS project, still need some time to upgrade it, and test it. the code how to test it from chat is in APT file in GD

--- WLS.py
+++ WLS.py
@@ -1,6 +1,7 @@
 import yfinance as yf
 import pandas as pd
 from fredapi import Fred
+from statsmodels.regression.linear_model import WLS
 import statsmodels.api as sm
 import numpy as np
 
@@ -22,6 +23,7 @@ jtsjol = fred.get_series('JTSJOL', observation_start='2007-01-01', observation_e
 # Download data for VVIX
 vvix = yf.download("^VVIX", start="2007-01-01", end="2023-03-13", interval="1d")['Close']
 
+# Download data for Recession Probabilities
 recprousm156n = fred.get_series('RECPROUSM156N', observation_start='2007-01-01', observation_end='2023-03-13')
 
 # Combine data into a single DataFrame
@@ -43,111 +45,103 @@ data_pct = data_pct.loc['2007-01-01':]
 # Rename columns
 data.columns = ['VIX', 'Consumer Confidence', 'High Yield Spread', 'Job Openings', 'VVIX', 'Recession Probabilities']
 
-# Rename columns
-data_pct.columns = ['VIX', 'Consumer Confidence', 'High Yield Spread', 'Job Openings', 'VVIX', 'Recession Probabilities']
-
-print(data_pct)
-
 # Save data to CSV file
 data.to_csv('data.csv', index=True)
 
-# Define the dependent variable as the historical returns of each asset
-returns = data_pct.dropna()
+# Create a new DataFrame with the independent variables
+X = sm.add_constant(data[['Consumer Confidence', 'High Yield Spread', 'Job Openings', 'VVIX',
+                          'Recession Probabilities']])
 
-# Define the independent variables as the macroeconomic factors
-X = sm.add_constant(
-    data_pct[['VIX', 'Consumer Confidence', 'High Yield Spread', 'Job Openings', 'VVIX', 'Recession Probabilities']].dropna())
+# Define weights for WLS method
+weights = 1 / data['VIX']
 
-# Use weighted least squares to estimate the betas for all assets simultaneously
-model = sm.WLS(returns, X, weights=1 / returns.std()).fit()
+# Fit a weighted linear regression model with VIX as the dependent variable
+model = WLS(data['VIX'], X, weights=weights).fit()
 
 # Print the model summary
 print(model.summary())
 
+# Rename columns for DB with returns
+data_pct.columns = ['VIX', 'Consumer Confidence', 'High Yield Spread', 'Job Openings', 'VVIX',
+                    'Recession Probabilities']
+
+# Replace any zeros, infinities, and negative infinities in the DataFrame data_pct with NaN values
+data_pct.replace([0, np.inf, -np.inf], np.nan, inplace=True)
+
+# Forward-fill any missing values
+data_pct = data_pct.ffill()
+
+# Backward-fill any missing values
+data_pct = data_pct.bfill()
+
+data_pct.to_csv('data_pct.csv', index=True)
+
+# Define the dependent variable as the historical returns of each asset
+returns = data_pct[['VIX']]
+
+# Define the independent variables as the macroeconomic factors
+independent_variables = data_pct[['Consumer Confidence', 'High Yield Spread', 'Job Openings', 'VVIX',
+                                  'Recession Probabilities']]
+
+# Use a multiple linear regression model to estimate the beta coefficients for each asset
+betas = pd.DataFrame(index=returns.columns, columns=independent_variables.columns)
+for asset in returns.columns:
+    model = WLS(returns[asset], sm.add_constant(independent_variables), weights=weights).fit()
+    betas.loc[asset] = model.params[1:]
+
 # Save the beta coefficients for each asset
-betas = pd.DataFrame(model.params[1:], columns=['Beta'])
-betas.index = returns.columns[1:]
 betas.to_csv('betas.csv', index=True)
 
 # Load the beta coefficients for each asset
 betas = pd.read_csv('betas.csv', index_col=0)
 
 # Download data on U.S. Treasury Securities at 1-Year Constant Maturity
-
 dtb3 = fred.get_series('dtb3', observation_start='2007-01-01', observation_end='2023-03-13')
+
 # Resample the data to a monthly frequency
-
 dtb3_monthly = dtb3.resample('M').last()
+
 # Convert yearly rate to monthly rate
+dtb3_monthly = ((1 + dtb3_monthly)**(1/52)) - 1
 
-dtb3_monthly = ((1 + dtb3_monthly) ** (1 / 52)) - 1
 # Forward fill missing values
-
 dtb3_monthly = dtb3_monthly.ffill()
 
 dtb3_monthly.to_csv('dtb3_monthly.csv')
+
 # Merge data_pct and dtb3_monthly using merge_asof
-
 data_pct_rf = pd.merge_asof(data_pct, dtb3_monthly.to_frame(), left_index=True, right_index=True, direction='forward')
+
 # Fill missing values with the last available value
-
 data_pct_rf.fillna(method='ffill', inplace=True)
-# Rename the last column to 'risk-free rate'
 
+# Rename the last column to 'risk-free rate'
 data_pct_rf = data_pct_rf.rename(columns={data_pct_rf.columns[-1]: 'risk free rate'})
 
 data_pct_rf.to_csv('data_pct_rf.csv')
-# Calculate excess returns for each asset
 
+# Calculate excess returns for each asset
+excess_returns = data
 excess_returns = data_pct_rf.subtract(dtb3_monthly.values[0], axis=0)
 
 excess_returns.to_csv('excess_returns.csv', index=True)
-# Calculate average excess returns for each factor
 
+# Calculate average excess returns for each factor
 avg_excess_returns = {}
 for factor in excess_returns.columns[1:]:
     excess_returns_rf = excess_returns[factor] - dtb3_monthly.values.flatten()
     avg_excess_returns[factor] = excess_returns_rf.mean()
 
 print(f'avg_excess_returns{avg_excess_returns}')
-# Calculate historical risk premiums
 
+# Calculate historical risk premiums
 risk_premiums = {}
 for factor in avg_excess_returns.keys():
     risk_premiums[factor] = avg_excess_returns[factor]
+
 # Print the historical risk premiums
-
 print(f'risk_premiums: {risk_premiums}')
-# Load data
 
-data_pct_rf = pd.read_csv('data_pct_rf.csv', index_col=0, parse_dates=True)
-# Define the dependent variable as the historical excess returns of each asset
-
-excess_returns = data_pct_rf.iloc[:, :-1]
-excess_returns = excess_returns.subtract(data_pct_rf.iloc[:, -1], axis=0)
-excess_returns.columns = excess_returns.columns.str.replace(' ', '_')
-excess_returns.dropna(inplace=True)
-# Define the independent variables as the macroeconomic factors
-
-independent_variables = data_pct_rf.iloc[:, -1:]
-independent_variables.columns = independent_variables.columns.str.replace(' ', '_')
-independent_variables.dropna(inplace=True)
-# Define the weights for the weighted least squares regression
-
-weights = pd.DataFrame(1 / excess_returns.var(axis=0), index=excess_returns.columns, columns=['Weight'])
-# Fit a weighted least squares regression model with excess returns as the dependent variable and macroeconomic
-# factors as the independent variables
-
-model = sm.WLS(excess_returns, sm.add_constant(independent_variables), weights=weights).fit()
-# Print the model summary
-
-print(model.summary())
-# Calculate the betas for each asset
-
-betas = model.params[1:]
-# Save the betas to a CSV file
-
-betas.to_csv('betas.csv', index=True)
 # Calculate the expected returns for each asset using the APT model formula
 expected_returns = pd.DataFrame(index=betas.index, columns=['Expected Return'])
 for asset in betas.index:
@@ -173,28 +167,3 @@ comparison.columns = ['Actual Return', 'Expected Return']
 comparison['Difference'] = comparison['Actual Return'] - comparison['Expected Return']
 print(comparison)
 
-# Run a multivariate regression to estimate the betas for all assets simultaneously
-model = sm.WLS(data_pct_rf.drop(columns=['risk free rate']), sm.add_constant(independent_variables),
-               weights=np.sqrt(dtb3_monthly.values.flatten())).fit()
-
-# Print the model summary
-print(model.summary())
-
-# Calculate the expected returns for each asset using the estimated betas and historical risk premiums
-expected_returns = pd.DataFrame(index=betas.index, columns=['Expected Return'])
-for asset in betas.index:
-    expected_return = dtb3_monthly.iloc[-1]
-    for factor in betas.columns:
-        premium = risk_premiums[factor]
-        beta = model.params[factor]
-        expected_return += beta * premium
-    expected_returns.loc[asset, 'Expected Return'] = expected_return
-
-# Print the expected returns for each asset
-print(expected_returns)
-
-# Compare actual and expected returns
-comparison = pd.concat([actual_returns, expected_returns], axis=1)
-comparison.columns = ['Actual Return', 'Expected Return']
-comparison['Difference'] = comparison['Actual Return'] - comparison['Expected Return']
-print(comparison)
